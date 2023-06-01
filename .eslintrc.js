679ef5bb4c10f562efe79645dd27d668ec934fe0
nakamura0907 <nakamura990907@gmail.com>
2023-06-01 15:18:49 +0900

    静的解析ツール導入

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {},
+};
