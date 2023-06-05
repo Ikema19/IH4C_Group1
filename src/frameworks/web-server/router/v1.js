0460f2fcd3b097acf68d3d7c89fe4fc375c65a90
nakamura0907 <nakamura990907@gmail.com>
2023-06-05 15:42:36 +0900

    仮のレスポンスフォーマット作成

--- src/frameworks/web-server/router/v1.js
+++ src/frameworks/web-server/router/v1.js
@@ -11,21 +11,99 @@ import express from "express";
 const v1Router = express.Router();
 
 v1Router.get("/spots", (_, res) => {
-  fetchSpots();
-  res.status(200).json({ message: "Hello from v1" });
+  const result = fetchSpots();
+  console.log(result);
+
+  res.status(200).send({
+    spots: [
+      {
+        id: "1",
+        name: "Spot 1",
+        description: "Description 1",
+        photo: "https://example.com/photo1.jpg",
+        geometry: {
+          location: {
+            lat: 11.111111,
+            lng: 11.111111,
+          },
+        },
+      },
+    ],
+  });
 });
 v1Router.get("/spots/:spotId", (_, res) => {
-  fetchSpotById();
-  res.status(200).json({ message: "Hello from v1" });
+  const result = fetchSpotById();
+  console.log(result);
+
+  res.status(200).send({
+    spot: {
+      id: "1",
+      name: "Spot 1",
+      description: "Description 1",
+      photo: "https://example.com/photo1.jpg",
+      geometry: {
+        location: {
+          lat: 11.111111,
+          lng: 11.111111,
+        },
+      },
+    },
+  });
 });
 
 v1Router.get("/courses/models", (_, res) => {
-  fetchModelCourses();
-  res.status(200).json({ message: "Hello from v1" });
+  const result = fetchModelCourses();
+  console.log(result);
+
+  res.status(200).send({
+    courses: [
+      [
+        {
+          id: "1",
+          name: "Sports1",
+          photo: "https://example.com/photo1.jpg",
+        },
+        {
+          id: "2",
+          name: "Sports2",
+          photo: "https://example.com/photo2.jpg",
+        },
+      ],
+    ],
+  });
 });
 v1Router.get("/courses/models/:modelId", (_, res) => {
-  fetchModelCourseById();
-  res.status(200).json({ message: "Hello from v1" });
+  const result = fetchModelCourseById();
+  console.log(result);
+
+  res.status(200).send({
+    course: [
+      {
+        id: "1",
+        name: "Sports1",
+        description: "Description 1",
+        photo: "https://example.com/photo1.jpg",
+        geometry: {
+          location: {
+            lat: 11.111111,
+            lng: 11.111111,
+          },
+        },
+      },
+      {
+        id: "2",
+        name: "Sports2",
+        description: "Description 2",
+        photo: "https://example.com/photo2.jpg",
+        geometry: {
+          location: {
+            lat: 22.222222,
+            lng: 22.222222,
+          },
+        },
+      },
+    ],
+  });
 });
 
 export { v1Router };
